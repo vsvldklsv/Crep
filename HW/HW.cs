084a7862fa1e35fa1ca376e16b842d627bdfb638
esceper <vsevolod.kylosov@gmail.com>
2014-09-10 10:13:31 +0600

    First commit

---
+++ HW/HW.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
+
